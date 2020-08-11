76877fb39799ad74f224d2d4eb8a61c1a3fc6a22
LSonvico <lucassonvico@gmail.com>
2020-08-11 17:03:44 -0300

    GIT

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+  "name": "DevTools",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "dependencies": {
+    "moment": "^2.27.0"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
